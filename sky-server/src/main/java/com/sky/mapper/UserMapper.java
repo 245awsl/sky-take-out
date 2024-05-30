8b4a0dd163cdb8ab287e6166d7d48a12baaef9f5
Jasper Yang <2714165146@qq.com>
2024-05-30 10:21:50 +0800

    小程序登录接口代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,26 @@
+package com.sky.mapper;
+
+import com.sky.annotation.AutoFill;
+import com.sky.entity.User;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Service;
+
+@Mapper
+public interface UserMapper {
+
+    /**
+     * 根据openid查询用户
+     * @param openid
+     * @return
+     */
+    @Select("select * from user where openid = #{openid}")
+    User getByIOpenId(String openid);
+
+    /**
+     * 插入用户后还要返回主键值
+     * @param user
+     */
+    void insert(User user);
+}
